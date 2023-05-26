672aa5abaf23ce81e08b650fc48cb24f6c69067d
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-26 13:22:51 +0200

    add vite-plugin-eslint (just in case)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import eslint from "vite-plugin-eslint";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     open: true,
   },
