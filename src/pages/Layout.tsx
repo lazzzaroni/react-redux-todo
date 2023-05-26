d61632487a0aef98b2d93409336e0c670125aaf3
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-26 18:31:52 +0200

    style Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,8 +2,13 @@ import { Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
-    <div>
-      <Outlet />
+    <div className="h-screen bg-white dark:bg-gray-900">
+      <header className="bg-slate-50 p-4 text-center text-xl font-semibold text-gray-900 dark:bg-gray-800 dark:text-gray-300 sm:text-2xl">
+        React + Redux Toolkit ToDo App
+      </header>
+      <div className="container mx-auto ">
+        <Outlet />
+      </div>
     </div>
   );
 }
