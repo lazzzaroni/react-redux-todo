f56607b7b7d90180d82b7440480b8c000d0345b4
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-26 19:48:45 +0200

    style AddTask

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -14,16 +14,29 @@ export function AddTask() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username" />
-      <input
-        id="username"
-        type="text"
-        value={value}
-        placeholder="Add task"
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button>Submit</button>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center justify-center sm:flex-row"
+    >
+      <div>
+        <label
+          htmlFor="username"
+          className="mx-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Add task
+        </label>
+        <input
+          id="username"
+          type="text"
+          value={value}
+          placeholder="e.g. Buy new MacBook"
+          onChange={(e) => setValue(e.target.value)}
+          className="m-1 block w-60 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:w-80"
+        />
+      </div>
+      <button className="m-1 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 sm:mt-7">
+        Submit
+      </button>
     </form>
   );
 }
