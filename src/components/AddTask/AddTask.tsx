898e7da108c35cfba31d41c51dc8785845a84c22
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-27 21:40:53 +0200

    refactor styles

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -16,25 +16,20 @@ export function AddTask() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex w-8/12 flex-col items-center justify-center sm:flex-row"
+      className="mb-4 flex flex-col items-center justify-center sm:w-96 sm:flex-row "
     >
-      <div>
-        <label
-          htmlFor="username"
-          className="mx-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Add task
-        </label>
+      <div className="w-64 sm:w-full">
+        <label htmlFor="username"></label>
         <input
           id="username"
           type="text"
           value={value}
-          placeholder="e.g. Buy new MacBook"
+          placeholder="Add new task"
           onChange={(e) => setValue(e.target.value)}
-          className="m-1 block w-60 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:w-80"
+          className="block h-10 w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
         />
       </div>
-      <button className="m-1 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 sm:mt-7">
+      <button className="mt-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 sm:ml-3 sm:mt-0">
         Submit
       </button>
     </form>
--- src/components/AllTasks/AllTasks.tsx
+++ src/components/AllTasks/AllTasks.tsx
@@ -6,15 +6,10 @@ export function AllTasks() {
   const tasks = useAppSelector(selectTasks);
 
   return (
-    <>
-      <h2 className="mb-2 mt-4 text-xl font-medium text-gray-900 dark:text-gray-100">
-        Task List
-      </h2>
-      <ul className="flex w-10/12 flex-col items-center justify-center sm:w-8/12">
-        {tasks.map((task) => (
-          <Task key={task.id} {...task} />
-        ))}
-      </ul>
-    </>
+    <ul className="flex w-10/12 flex-col items-center justify-center sm:w-8/12">
+      {tasks.map((task) => (
+        <Task key={task.id} {...task} />
+      ))}
+    </ul>
   );
 }
