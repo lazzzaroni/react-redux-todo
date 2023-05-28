951ba2af0ced0a500c0832133fd4996cac518bce
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-05-28 20:47:07 +0200

    I think that's it...

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
